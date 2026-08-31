715f4044bbfbc33c6215e13968783c3fa09246e2
Pritam Kumar Choudhary <Schrodinger2027@gmail.com>
2026-08-31 19:41:38 +0530

    Time: 569 ms (16.41%) | Memory: 243.4 MB (46.80%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,64 @@
+class Solution {
+    public:
+    int merge(vector<int>& nums, int l, int m, int r) {
+        int ans = 0;
+        int j = m + 1;
+
+        for(int i = l; i <= m; i++) {
+            while(j <= r && nums[i] > 2LL * nums[j])
+            j++;
+
+            ans += j - (m + 1);
+        }
+
+        vector<int> v;
+        int i = l;
+        j = m + 1;
+
+        while(i <= m && j <= r) {
+            if(nums[i] <= nums[j]) {
+                v.push_back(nums[i]);
+                i++;
+            }
+            else {
+                v.push_back(nums[j]);
+                j++;
+            }
+        }
+
+        while(i <= m) {
+            v.push_back(nums[i]);
+            i++;
+        }
+
+        while(j <= r) {
+            v.push_back(nums[j]);
+            j++;
+        }
+
+        for(int i = l; i <= r; i++)
+        nums[i] = v[i - l];
+
+        return ans;
+    }
+
+    int solve(vector<int>& nums, int l, int r) {
+        if(l >= r)
+        return 0;
+
+        int m = l + (r - l) / 2;
+
+        int ans = 0;
+
+        ans += solve(nums, l, m);
+        ans += solve(nums, m + 1, r);
+        ans += merge(nums, l, m, r);
+
+        return ans;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        return solve(nums, 0, nums.size() - 1);
+    }
+};
+   
